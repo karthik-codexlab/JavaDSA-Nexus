778e14b13b9af817d73eae6e3998a4424a4c489c
karthik-techlabz <karthik.tech0x1@gmail.com>
2025-12-09 23:35:46 +0530

    Add Employee class with attributes and methods

---
+++ java-streams-advanced/src/com/example/streams/StreamAdvancedOperations.java
@@ -0,0 +1,33 @@
+package com.example.streams;
+
+public class Employee {
+    private final int id;
+    private final String name;
+    private final String department;
+    private final double salary;
+
+    public Employee(int id, String name, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() { return id; }
+
+    public String getName() { return name; }
+
+    public String getDepartment() { return department; }
+
+    public double getSalary() { return salary; }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
