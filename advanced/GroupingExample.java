73f162f34da0fc8712d9b3b59f34a1892763186a
karthik-techlabz <karthik.tech0x1@gmail.com>
2025-12-12 23:28:21 +0530

    Add GroupingExample class to demonstrate grouping by length

---
+++ advanced/GroupingExample.java
@@ -0,0 +1,16 @@
+package advanced;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class GroupingExample {
+    public static void main(String[] args) {
+        List<String> words = List.of("java", "javascript", "python", "perl", "php");
+
+        Map<Integer, List<String>> byLength =
+                words.stream()
+                     .collect(Collectors.groupingBy(String::length));
+
+        System.out.println(byLength);
+    }
+}
