9520407c13a2587eb7a69b1bbafcc1bb86287cc9
karthik-techlabz <karthik.tech0x1@gmail.com>
2025-12-14 22:53:23 +0530

    Add ExecutorServiceDemo for concurrent task execution

---
+++ src/java-concurrency/ExecutorServiceDemo.java
@@ -0,0 +1,20 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceDemo {
+
+    public static void main(String[] args) {
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        for (int i = 1; i <= 5; i++) {
+            int taskId = i;
+            executor.submit(() ->
+                System.out.println("Task " + taskId + " executed by " +
+                        Thread.currentThread().getName())
+            );
+        }
+
+        executor.shutdown();
+    }
+}
